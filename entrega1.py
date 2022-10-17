0cde438b9f97cd15747228ddc5c524473db8663d
diego <diegocarpbressan@gmail.com>
2022-10-17 18:51:25 -0300

    optimizando el actions

--- entrega1.py
+++ entrega1.py
@@ -9,17 +9,14 @@ from simpleai.search import (
 )
 
 #El estado lo representamos con: 
-# -Primera tupla donde guardamos la posición del jugador
-# -Segunda tupla (Lista de las posiciones actuales de las cajas en el mapa)
-# -Tercera tupla lista inicialmente vacia (iremos almacenando y sacando las cajas de la lista a medida que se encuentren en su posición)
-# -Cuarta tupla (Lista de posiciones actuales de lugares correctos)
-# -Quinta tupla lista inicialmente vacia (iremos almacenando y sacando los lugares correctos para saber cuales quedan)
+# - Primera tupla donde guardamos la posición del jugador
+# - Segunda tupla (Lista de las posiciones actuales de las cajas en el mapa)
+# - Tercera tupla lista inicialmente vacia (iremos almacenando y sacando las cajas de la lista a medida que se encuentren en su posición)
+# - La cantidad maxima de mivimientos
 initial = (
 
-    (1, 4),
+    (4, 5),
     ((2, 3), (3, 4), (4, 4), (6, 1), (6, 3), (6, 4), (6, 5)),
-    (),
-    ((2, 1), (3, 5), (4, 1), (5, 4), (6, 3), (7, 4), (6, 6)),
     (),
     50
 
@@ -32,83 +29,62 @@ obstaculos = (
 (8, 0), (8, 1), (8, 2), (8, 3), (8, 4), (8, 5), (8, 6), (8, 7),
 )
 
+objetivos = (
+    (2, 1), (3, 5), (4, 1), (5, 4), (6, 3), (7, 4), (6, 6),
+)
 
 from simpleai.search.viewers import WebViewer, BaseViewer
 
 class Socoban(SearchProblem):
 
     def cost(self, state, action, state2):
-        
         return 1
 
     def is_goal(self, state):
-
-        if (state[1] == () and state[3] == ()):
+        if (state[1] == () and state[3] >= 0):
             return True 
 
     def actions(self, state):
-
-        pos_jugador, lista_pos_cajas, lista_resultado, lista_pos_correctos, lista_resultado2, _ = state
+        pos_jugador, lista_pos_cajas, lista_resultado, max_mov = state
         pos_fila = pos_jugador[0]
         pos_columna = pos_jugador[1]
 
-        #La lista de acciones esta compuesta por 3 tuplas en el caso que se deba mover una caja
-        #1- El movimiento del personaje
-        #2- El movimiento de la caja
-        #3- 'Empujar' si hay que empujar una caja o 'Mover' si solo hay que mover el personaje
         lista_acciones = []
-        
-        movimientos = []
-        movimientos.append((pos_fila - 1, pos_columna)) #arriba
-        movimientos.append((pos_fila + 1, pos_columna)) #abajo
-        movimientos.append((pos_fila, pos_columna - 1)) #izquierda
-        movimientos.append((pos_fila, pos_columna + 1)) #derecha
 
-        for indicemov, movimiento in enumerate(movimientos):
-            #Pregunto si el movimiento es posible y no hay pared..
-            if movimiento not in obstaculos:
-                #Pregunto si me encuentro pegado a una caja
-                if movimiento in lista_pos_cajas:
-                    if indicemov == 0: #Si esta parado debajo de una caja
-                        auxfila, auxcolumna = movimiento
-                        band = 0
-                        #Recorro la lista de obstaculos y pregunto si del otro lado de la caja hay un obstaculo
-                        for obstaculo in obstaculos:
-                            if (auxfila - 1, auxcolumna) == obstaculo:
-                                band = 1
-                                break
-                        #Si no hay ningun obstaculo, puedo mover la caja
-                        if band == 0:
-                            lista_acciones.append((movimiento, (auxfila - 1, auxcolumna), 'Empujar'))
-                    if indicemov == 1: #Si esta parado arriba de una caja
-                        auxfila, auxcolumna = movimiento
-                        band = 0
-                        for obstaculo in obstaculos:
-                            if (auxfila + 1, auxcolumna) == obstaculo:
-                                band = 1
-                                break
-                        if band == 0:
-                            lista_acciones.append((movimiento, (auxfila + 1, auxcolumna), 'Empujar'))
-                    if indicemov == 2: #Si esta parado a la Derecha de una caja
-                        auxfila, auxcolumna = movimiento
-                        band = 0
-                        for obstaculo in obstaculos:
-                            if (auxfila, auxcolumna - 1) == obstaculo:
-                                band = 1
-                                break
-                        if band == 0:
-                            lista_acciones.append((movimiento, (auxfila, auxcolumna - 1), 'Empujar'))
-                    if indicemov == 3: #Si esta parado a la Izquierda de una caja
-                        auxfila, auxcolumna = movimiento
-                        band = 0
-                        for obstaculo in obstaculos:
-                            if (auxfila, auxcolumna + 1) == obstaculo:
-                                band = 1
-                                break
-                        if band == 0:
-                            lista_acciones.append((movimiento, (auxfila, auxcolumna + 1), 'Empujar'))
-                else:
-                    lista_acciones.append((movimiento, 'Mover'))
+        # Armo la lista de las posibles acciones
+
+        #   Arriba
+        if (pos_fila - 1, pos_columna) not in obstaculos: # Pregunto si hay un obstaculo
+            if (pos_fila - 1, pos_columna) in lista_pos_cajas: # Pregunto si hay una caja
+                if (pos_fila - 2, pos_columna) not in obstaculos: # Pregunto si detras de la caja hay un obstaculo
+                    lista_acciones.append(('E', 'Arriba', (pos_fila - 1, pos_columna)))
+            else:
+                lista_acciones.append(('M','Arriba'))
+
+        #   Abajo
+        if (pos_fila + 1, pos_columna) not in obstaculos:
+            if (pos_fila + 1, pos_columna) in lista_pos_cajas:
+                if (pos_fila + 2, pos_columna) not in obstaculos:
+                    lista_acciones.append(('E', 'Abajo', (pos_fila + 1, pos_columna)))
+            else:
+                lista_acciones.append(('M','Abajo'))
+
+        #   Izquierda
+        if (pos_fila, pos_columna - 1) not in obstaculos:
+            if (pos_fila, pos_columna - 1) in lista_pos_cajas:
+                if (pos_fila, pos_columna - 2) not in obstaculos:
+                    lista_acciones.append(('E', 'Izquierda', (pos_fila, pos_columna - 1)))
+            else:
+                lista_acciones.append(('M','Izquierda'))
+
+        #   Derecha
+        if (pos_fila, pos_columna + 1) not in obstaculos:
+            if (pos_fila, pos_columna + 1) in lista_pos_cajas:
+                if (pos_fila, pos_columna + 2) not in obstaculos:
+                    lista_acciones.append(('E', 'Derecha', (pos_fila, pos_columna + 1)))
+            else:
+                lista_acciones.append(('M','Derecha'))
+
         return lista_acciones
 
     def result(self, state, action):
