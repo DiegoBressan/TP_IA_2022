0c83f38084d642e6e6abca63272a2e7ae9574a65
DESKTOP-4BGVB69\David <davidgiovannini90@gmail.com>
2022-10-24 18:07:40 -0300

    Solucion CSP

--- entrega2.py
+++ entrega2.py
@@ -62,91 +62,101 @@ for variable in variables:
 restricciones = []
 
 
-#def Devolver_listas(valor):
+def Devolver_listas(variable, valor):
     
-#    lista_paredes = []
-#    lista_cajas = []
-#    lista_objetivos = []
-#    player = []
-#    for variable in valor:
-#        if variable[0] == 'P':
-#            lista_paredes.append(variable)
-#        elif variable[0] == 'C':
-#            lista_cajas.append(variable)
-#        elif variable[0] == 'O':
-#            lista_objetivos.append(variable)
-#        else:
-#            player.append(variable)
+    lista_paredes = []
+    lista_cajas = []
+    lista_objetivos = []
 
-#    return (lista_paredes, lista_cajas, lista_objetivos, player)
+    for indice, v in enumerate(variable):
+        if v[0] == 'Pared':
+            lista_paredes.append(valor[indice])
+        elif v[0] == 'Cajas':
+            lista_cajas.append(valor[indice])
+        elif v[0] == 'Objetivos':
+            lista_objetivos.append(valor[indice])
+        elif v[0] == 'Persona':
+            player = valor[indice]
+
+    return (lista_paredes, lista_cajas, lista_objetivos, player)
 
 def Paredes_con_Paredes(variables, values): # Que no hayan paredes en la misma posicion
     
-    paredes, _, _, _ = Devolver_listas(values)
+    paredes, _, _, _ = Devolver_listas(variables, values)
 
     if len(paredes) == len(set(paredes)):
-        return False # Todos los valores de paredes son diferentes
+        return True # Todos los valores de paredes son diferentes
     else:
-        return True # Hay 2 valores que son iguales, se viola la restriccion
+        return False # Hay 2 valores que son iguales, se viola la restriccion
 
 restricciones.append((variables, Paredes_con_Paredes))
 
 def Cajas_con_cajas(variables, values): # Que no hayan cajas en la misma posicion
 
-    _, cajas, _, _ = Devolver_listas(values)
+    _, cajas, _, _ = Devolver_listas(variables, values)
 
     if len(cajas) == len(set(cajas)):
-        return False # Todos los valores de cajas son diferentes
+        return True # Todos los valores de cajas son diferentes
     else:
-        return True # Hay 2 valores que son iguales, se viola la restriccion
+        return False # Hay 2 valores que son iguales, se viola la restriccion
 
 restricciones.append((variables, Cajas_con_cajas))
 
+def Objetivos_con_objetivos(variables, values): # Que no hayan objetivos en la misma posicion
+
+    _, _, objetivos, _ = Devolver_listas(variables, values)
+
+    if len(objetivos) == len(set(objetivos)):
+        return True # Todos los valores de objetivos son diferentes
+    else:
+        return False # Hay 2 valores que son iguales, se viola la restriccion
+
+restricciones.append((variables, Objetivos_con_objetivos))
+
 def Cajas_con_paredes(variables, values): # Que no hayan cajas en la misma posicion que las paredes
 
-    paredes, cajas, _, _ = Devolver_listas(values)
+    paredes, cajas, _, _ = Devolver_listas(variables, values)
 
     for caja in cajas:
         if caja in paredes:
-            return True # Hay una caja en una pared, se viola la restriccion
+            return False # Hay una caja en una pared, se viola la restriccion
 
-    return False # No hay cajas en las paredes
+    return True # No hay cajas en las paredes
 
 restricciones.append((variables, Cajas_con_paredes))
 
 def Paredes_con_objetivos(variables, values): # Que no hayan Paredes en los objetivos
 
-    paredes, _, objetivos, _ = Devolver_listas(values)
+    paredes, _, objetivos, _ = Devolver_listas(variables, values)
 
     for objetivo in objetivos:
         if objetivo in paredes:
-            return True # Hay un objetivo en una pared, se viola la restriccion
+            return False # Hay un objetivo en una pared, se viola la restriccion
     
-    return False # No hay objetivos en las paredes
+    return True # No hay objetivos en las paredes
 
 restricciones.append((variables, Paredes_con_objetivos))
 
 def Player_con_CajasParedes(variables, values): # Que el player no este en una pared o una caja
 
-    paredes, cajas, _, player = Devolver_listas(values)
+    paredes, cajas, _, player = Devolver_listas(variables, values)
 
-    for jugador in player:
-        if (jugador in paredes) or (jugador in cajas):
-            return True # El jugador esta en la posicion de una caja o una pared, se viola la restriccion
+    if (player in paredes) or (player in cajas):
+        return False # El jugador esta en la posicion de una caja o una pared, se viola la restriccion
 
-    return False # El jugador no se encuentra en la posicion de una pared o caja
+    return True # El jugador no se encuentra en la posicion de una pared o caja
 
 restricciones.append((variables, Player_con_CajasParedes))
 
 
 #--------------------------------------------------------
-#problema = CspProblem(variables, dominios, restricciones)
-#result = backtrack(problema)
-#print('Resultado: ')
-#print(result)
+problema = CspProblem(variables, dominios, restricciones)
+result = backtrack(problema)
+print('Resultado: ')
+print(result)
 #--------------------------------------------------------
 
 #print(variables)
-print(dominios)
+#print(dominios)
 #print(lista_grilla)
 #print(nuevalista)
